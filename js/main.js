e369df418f72904b2c4f9b041e8e938e76a42140
Katie Corcoran <corcorankt1@gmail.com>
2016-03-29 20:37:21 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function yourLocation() {
+	var locale = prompt("Where are you from?", "Detroit");
+
+	if (locale != null)	{
+		document.getElementById("location").innerHTML = locale + "? That's great! I hear it's nice there.";
+		console.log(locale);
+	}
+}
